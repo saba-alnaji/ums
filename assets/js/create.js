2950486127d4a954c1c0563455931ae67633746b
saba-alnaji <sabanaji930@gmail.com>
2026-02-08 00:01:00 +0200

    create get delete creat

---
+++ assets/js/create.js
@@ -0,0 +1,8 @@
+const createUserForm = document.forms['AddUserForm'];
+createUserForm.addEventListener("submit",async(e)=>{
+    e.preventDefault();
+    const formData = new FormData(createUserForm);
+   
+    const response = await axios.post(`https://ums12.runasp.net/api/users`,formData);
+    console.log(response);
+});
---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const getUsers = async () => {
+    const response = await axios.get(`https://ums12.runasp.net/api/users?limit=1000`);
+    return response.data;
+}
+const displayUsers = async () => {
+    const result = await getUsers();
+    const users = result.users.map((user) => {
+        return `
+        <tr>
+          <td>${user.name}</td> 
+          <td> <img src="${user.imageUrl}" width=""100px/></td>
+          <td>
+            <button class="btn btn-outline-danger" onclick=deleteUser(${user.id})>Delete</button>
+          
+          </td>
+
+        </tr>
+        `
+    }).join(" ")
+    console.log(users);
+    document.querySelector(".users .users-data").innerHTML = users;
+} 
+displayUsers();
+const deleteUser =async(id)=>{
+    const response = await axios.delete(`https://ums12.runasp.net/api/users/ ${id}`);
+    console.log(response);
+}
